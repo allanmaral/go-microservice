414b66e71b42d6a0425978114bfd03182205db04
Allan Amaral <allanmaralr@gmail.com>
2022-03-09 20:35:51 -0300

    test: ensure GetUser return the the correc application error

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+
+	"github.com/allanmaral/go-microservice/mvc/utils"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	var input int64 = 0
+	want := &utils.ApplicationError{
+		Message:    fmt.Sprintf("user %v was not found", input),
+		StatusCode: http.StatusNotFound,
+		Code:       "NotFound",
+	}
+
+	usr, got := GetUser(input)
+
+	assert.Nil(t, usr)
+	assert.EqualValues(t, got, want)
+}
+
+func TestGetUserValidId(t *testing.T) {
+	var input int64 = 123
+	want := &User{Id: 123, FirstName: "Allan", LastName: "Ribeiro", Email: "email@email.com"}
+
+	got, err := GetUser(input)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, got, want)
+}
